b10c5a9b9a31252c0c90a40803e88f0211d51d19
dubie <dubie@dubies-Mac.local>
2017-11-07 11:08:42 -0800

    Initial Commit

---
+++ SlackClone/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SlackClone
+//
+//  Created by dubie on 11/7/17.
+//  Copyright © 2017 dubay. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
